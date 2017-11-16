edfcca535dee9b3e8c6574b4cd0eb4bebe29bde1
zhangyuwei <570146800@qq.com>
2017-11-16 13:46:52 +0800

    : feat(collision): completed collision function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+// table detail
+var tableWidth = 505;
+var tableHeight = 606;
+
+// move element details
+var generalWidth = 101;
+var generalheight = 83;
+var offsetY = 20;
+
+// enemies move rate
+var enemiesMoveRate = 20;
+
 // Enemies our player must avoid
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
@@ -6,8 +18,8 @@ var Enemy = function(x,y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x * 101;
-    this.y = y * 83 - 20;
+    this.x = x * generalWidth - generalWidth;
+    this.y = y * generalheight - offsetY;
 };
 
 // Update the enemy's position, required method for game
@@ -16,7 +28,16 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = (dt * 50 + this.x)%505;
+    var x = (dt * enemiesMoveRate + this.x);
+
+    if (x > tableWidth) {
+        x = -generalWidth;
+    }
+
+    this.x = x;
+
+    // 如果玩家的位置和 emeny 的面积虫重合那么游戏终止
+    CheckOverLap();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,13 +49,13 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(x,y) {
-   this.x = x * 101;
-   this.y = y * 83 - 20;
+   this.x = x * generalWidth;
+   this.y = y * generalheight - offsetY;
    this.character = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(dt) {
-    dt = dt + 1;
+Player.prototype.update = function() {
+
 };
 
 Player.prototype.render = function() {
@@ -48,32 +69,86 @@ Player.prototype.handleInput = function(keyCode) {
             this.y = (this.y === 63 ? 5 * 83 -20 : this.y - 83);
             break;
         case "down":
-            this.y = (this.y + 83)%606;
+            this.y = (this.y === 395 ? 395 : this.y + 83);
             break;
         case "right":
             this.x = (this.x + 101)%505;
             break;
         case "left":
-            this.x = (this.x - 101 === -101 ? 404 : this.x - 101);
+            this.x = (this.x  === 0 ? 404 : this.x - 101);
             break;
         default:
         break;
     }
 };
 
+Player.prototype.resetPosition = function(x,y) {
+    this.x = x * generalWidth;
+    this.y = y * generalheight - offsetY;
+};
+
+
+function CheckOverLap() {
+    var isOverLap = false;
+
+    var playerX = player.x + generalWidth;
+    var playerY = player.y + generalheight;
+    var playerWidth = generalWidth;
+    var playerHeight = generalheight;
+
+    var width;
+    var height;
+
+    for (var i = 0; i < allEnemies.length; i++) {
+
+       var enemy = allEnemies[i];
+
+       var enemyX = enemy.x + generalWidth;
+       var enemyY = enemy.y + generalheight;
+       var enemyWidth = generalWidth;
+       var enemyHeight = generalheight;
+
+       var endX = max(playerX,enemyX);
+       var startX = min(player.x,enemy.x);
+       width = enemyWidth + playerWidth - (endX - startX);
+
+       var endY = max(playerY, enemyY);
+       var startY  = min(player.y,enemy.y);
+       height = enemyHeight + playerHeight - (endY - startY);
+
+       if (width > 0 && height > 0) {
+          isOverLap = true;
+          break ;
+       }
+    }
+
+    if (isOverLap) {
+        // touch the enemy
+        player.resetPosition(0,5);
+        isOverLap = false;
+        return true;
+    }else {
+        return false;
+    }
+};
+
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy(2,4);
+var enemy1 = new Enemy(0,4);
 var enemy2 = new Enemy(1,4);
 var enemy3 = new Enemy(3,1);
-var enemy4 = new Enemy(1,2);
+var enemy4 = new Enemy(0,2);
 var enemy5 = new Enemy(2,3);
-var enemy6 = new Enemy(3,2);
+var enemy6 = new Enemy(0,2);
 var enemy7 = new Enemy(2,2);
 var enemy8 = new Enemy(3,4);
-
 var allEnemies =[enemy1,enemy2,enemy3,enemy4,enemy5,enemy6,enemy7,enemy8];
+
+// var allEnemies =[enemy1];
+//
 var player = new Player(0,5);
 
 // This listens for key presses and sends the keys to your
---
+++ js/utils.js
@@ -0,0 +1,15 @@
+function max(number1 , number2) {
+    if (parseInt(number1) > parseInt(number2)) {
+        return number1;
+    }else {
+        return number2;
+    }
+}
+
+function min(number1 , number2) {
+    if (parseInt(number1) < parseInt(number2)) {
+        return number1;
+    }else {
+        return number2;
+    }
+}
