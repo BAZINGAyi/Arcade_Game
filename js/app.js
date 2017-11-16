e27d361ddbbc588f35f2e7ae2dba7fedaf689803
zhangyuwei <570146800@qq.com>
2017-11-16 15:25:53 +0800

    : feat(code_standard)： update code style

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+/********** init data **************/
+
 // table detail
 var tableWidth = 505;
 var tableHeight = 606;
+var tableRows = 5;
+var tableColumns = 6;
 
 // move element details
 var generalWidth = 101;
@@ -10,7 +14,14 @@ var offsetY = 20;
 // enemies move rate
 var enemiesMoveRate = 20;
 
-// Enemies our player must avoid
+/********** init end ****************/
+
+
+
+
+
+/********** enemy set **************/
+
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -22,6 +33,7 @@ var Enemy = function(x,y) {
     this.y = y * generalheight - offsetY;
 };
 
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -29,14 +41,13 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     var x = (dt * enemiesMoveRate + this.x);
-
     if (x > tableWidth) {
         x = -generalWidth;
     }
 
     this.x = x;
 
-    // 如果玩家的位置和 emeny 的面积虫重合那么游戏终止
+    // check whether overlap of player's square and enemy's square
     CheckOverLap();
 };
 
@@ -45,11 +56,17 @@ Enemy.prototype.render = function() {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/********** enemy end **************/
+
+
+
+
+
+/********** player set**************/
+
 var Player = function(x,y) {
    this.x = x * generalWidth;
+   // The Plater's position will on the bottom of table if y don't minus offset
    this.y = y * generalheight - offsetY;
    this.character = 'images/char-boy.png';
 };
@@ -66,16 +83,17 @@ Player.prototype.handleInput = function(keyCode) {
     switch(keyCode)
     {
         case "up":
-            this.y = (this.y === 63 ? 5 * 83 -20 : this.y - 83);
+            // Player won when the player arrive at river
+            this.y = (this.y === generalheight - offsetY ? tableRows * generalheight - offsetY : this.y - generalheight);
             break;
         case "down":
-            this.y = (this.y === 395 ? 395 : this.y + 83);
+            this.y = (this.y === tableRows * generalheight - offsetY ? tableRows * generalheight - offsetY : this.y + generalheight);
             break;
         case "right":
-            this.x = (this.x + 101)%505;
+            this.x = (this.x + generalWidth) % tableWidth;
             break;
         case "left":
-            this.x = (this.x  === 0 ? 404 : this.x - 101);
+            this.x = (this.x  === 0 ? tableWidth - generalWidth : this.x - generalWidth);
             break;
         default:
         break;
@@ -87,35 +105,46 @@ Player.prototype.resetPosition = function(x,y) {
     this.y = y * generalheight - offsetY;
 };
 
+/********** player end *************/
+
+
+
+
+
+/********** utils start **************/
 
 function CheckOverLap() {
     var isOverLap = false;
 
+    // get player-rectangle right bottom position
     var playerX = player.x + generalWidth;
     var playerY = player.y + generalheight;
     var playerWidth = generalWidth;
     var playerHeight = generalheight;
 
-    var width;
-    var height;
-
     for (var i = 0; i < allEnemies.length; i++) {
 
        var enemy = allEnemies[i];
 
+       // get enemy-rectangle right bottom position
        var enemyX = enemy.x + generalWidth;
        var enemyY = enemy.y + generalheight;
        var enemyWidth = generalWidth;
        var enemyHeight = generalheight;
 
-       var endX = max(playerX,enemyX);
-       var startX = min(player.x,enemy.x);
-       width = enemyWidth + playerWidth - (endX - startX);
+       // calculate overlap area
+       // get the max value of x in the player‘s and enemy
+       var endX = max(playerX, enemyX);
+       // get the minutes value of x in the player‘s and enemy
+       var startX = min(player.x, enemy.x);
+       // get overlap squate in the player‘s and enemy
+       var width = enemyWidth + playerWidth - (endX - startX);
 
        var endY = max(playerY, enemyY);
-       var startY  = min(player.y,enemy.y);
-       height = enemyHeight + playerHeight - (endY - startY);
+       var startY  = min(player.y, enemy.y);
+       var height = enemyHeight + playerHeight - (endY - startY);
 
+       // If you find overlap in the x and y directions，game is over
        if (width > 0 && height > 0) {
           isOverLap = true;
           break ;
@@ -123,8 +152,8 @@ function CheckOverLap() {
     }
 
     if (isOverLap) {
-        // touch the enemy
-        player.resetPosition(0,5);
+        // reset player position
+        player.resetPosition(0, 5);
         isOverLap = false;
         return true;
     }else {
@@ -132,28 +161,10 @@ function CheckOverLap() {
     }
 };
 
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var enemy1 = new Enemy(0,4);
-var enemy2 = new Enemy(1,4);
-var enemy3 = new Enemy(3,1);
-var enemy4 = new Enemy(0,2);
-var enemy5 = new Enemy(2,3);
-var enemy6 = new Enemy(0,2);
-var enemy7 = new Enemy(2,2);
-var enemy8 = new Enemy(3,4);
-var allEnemies =[enemy1,enemy2,enemy3,enemy4,enemy5,enemy6,enemy7,enemy8];
-
-// var allEnemies =[enemy1];
-//
-var player = new Player(0,5);
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
+
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -163,3 +174,30 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+/********** utils end ****************/
+
+
+
+
+
+
+/********** game start set**************/
+
+var allEnemies = [];
+var player;
+// randomly generate enemy
+(function generate() {
+    var enemyCount = Math.ceil(Math.random() * 5);
+    for (var i = enemyCount; i >= 0; i--) {
+        var x = randomNum(0, 4);
+        var y = randomNum(1, 4);
+        var enemy = new Enemy(x, y);
+        allEnemies.push(enemy);
+    }
+
+    var playerXPos = Math.ceil(Math.random() * 4);
+    player = new Player(playerXPos, 5);
+})();
+
+/********** game end *******************/
--- js/utils.js
+++ js/utils.js
@@ -13,3 +13,17 @@ function min(number1 , number2) {
         return number2;
     }
 }
+
+function randomNum(minNum,maxNum){
+    switch(arguments.length){
+        case 1:
+            return parseInt(Math.random()*minNum+1,10);
+        break;
+        case 2:
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10);
+        break;
+        default:
+            return 0;
+        break;
+    }
+}
